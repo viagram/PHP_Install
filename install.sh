fb8d1b1e14d53aaad203db20d5897a83d8244d0d
viagram <viagram@users.noreply.github.com>
2019-08-20 20:20:19 +0800

    fix some

--- install.sh
+++ install.sh
@@ -185,6 +185,7 @@ else
 			printnew -green "解压成功"
 		fi
 		CMAKE_CMD=$(command -v cmake)
+		[[ -z ${MAKE_CMD} ]] && CMAKE_CMD=/usr/bin/cmake
 		cd ${CMAKE_DIR}
 		./bootstrap
 		printnew -green "开始编译${CMAKE_DIR}..."
